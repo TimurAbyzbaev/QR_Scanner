5e88cc102507806f092bac55366ff1699f8c2bdd
Timur <timur@vlaga.pro>
2022-12-23 08:53:57 +0500

    Initial commit

---
+++ app/src/main/java/ru/abyzbaev/qr_scanner/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.abyzbaev.qr_scanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.abyzbaev.qr_scanner.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
